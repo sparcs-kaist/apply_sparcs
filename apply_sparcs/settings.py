74401c089dac1cda621957b2d71e16808d373fbc
zeakd <stuzeakd@kaist.ac.kr>
2017-02-25 18:29:03 +0900

    change naming. media -> static

--- apply_sparcs/settings.py
+++ apply_sparcs/settings.py
@@ -133,3 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'frontend/dist'),
+    os.path.join(BASE_DIR, 'frontend/static'),
+)
--- apply_sparcs/ui_dev/settings.py
+++ apply_sparcs/ui_dev/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/media/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'frontend/dist'),
-    os.path.join(BASE_DIR, 'frontend/media'),
+    os.path.join(BASE_DIR, 'frontend/static'),
 )
