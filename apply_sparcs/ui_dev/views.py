603bdf2050c86b1929ea4af6f0a0f20868733bde
zeakd <stuzeakd@kaist.ac.kr>
2017-02-25 22:32:40 +0900

    Update Simple View rendering logic

--- apply_sparcs/ui_dev/views.py
+++ apply_sparcs/ui_dev/views.py
@@ -1,36 +1,83 @@
 from django import template
 from django.http import HttpResponse
 from django.shortcuts import render
+import json
 
 def simple(request, path, ext=None):
     filename = path
     print(path)
     print(ext)
 
+    # if ext == None:
+    #     if path == '' or path[-1] == '/':
+    #         filename += 'index'
+    #     context = getContextFromFilename(filename)
+    #     try:
+    #         res = render(request, filename + '.html', context)
+    #     except IsADirectoryError:
+    #         try:
+    #             print(path)
+    #             filename = path + '/index'
+    #             print(filename)
+    #             res = render(request, filename + '.html')
+    #         except (template.TemplateDoesNotExist, PermissionError):
+    #             res = render(request, '404.html')
+    #     except template.TemplateDoesNotExist:
+    #         try:
+    #             filename = path + '.html'
+    #             res = render(request, filename)
+    #         except template.TemplateDoesNotExist:
+    #             res = render(request, '404.html')
+    # elif ext == 'html':
+    #     try:
+    #         res = render(request, filename + '.' + ext)
+    #     except template.TemplateDoesNotExist:
+    #         res = render(request, '404.html')
+    # else:
+    #     res = HttpResponse(status=400)
+
+
     if ext == None:
+        filename = path
+        print (filename)
         if path == '' or path[-1] == '/':
-            filename += 'index.html'
+            filename += 'index'
+        context = getContextFromFilename(filename)
+        print (filename)
+        print (context)
         try:
-            res = render(request, filename)
-        except IsADirectoryError:
+            res = render(request, filename + '.html', context)
+        except template.TemplateDoesNotExist as e:
+            print('except')
             try:
-                filename = path + '/index.html'
-                res = render(request, filename)
-            except (template.TemplateDoesNotExist, PermissionError):
-                res = render(request, '404.html')
-        except template.TemplateDoesNotExist:
-            try:
-                filename = path + '.html'
-                res = render(request, filename)
+                filename = path + '/index'
+                context = getContextFromFilename(filename)
+                res = render(request, filename + '.html', context)
             except template.TemplateDoesNotExist:
                 res = render(request, '404.html')
     elif ext == 'html':
+        filename = path
+        context = getContextFromFilename(filename)
         try:
-            res = render(request, filename + '.' + ext)
+            res = render(request, filename + '.html', context)
         except template.TemplateDoesNotExist:
             res = render(request, '404.html')
     else:
         res = HttpResponse(status=400)
 
-
     return res
+
+def getContextFromFilename(filename):
+    '''
+        컨텍스트 파일을 불러온다.
+    '''
+    try:
+        with open(filename + '.context.json') as context_data:
+            context = json.load(context_data)
+    except FileNotFoundError as e:
+        # print(e)
+        context = {}
+    except json.decoder.JSONDecodeError as e:
+        context = None
+        # print(e)
+    return context
