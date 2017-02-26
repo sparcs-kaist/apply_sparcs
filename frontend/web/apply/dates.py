899ac7935ecaf75fdce7b7c4bcdd77eb9ee16374
zeakd <stuzeakd@kaist.ac.kr>
2017-02-27 01:55:11 +0900

    dates initializer and index edit

---
+++ frontend/web/apply/dates.py
@@ -0,0 +1,172 @@
+init = [
+  {
+    "dateTitle": "3 / 7 Tuesday",
+    "dates": [
+      {
+        "time": "19:00",
+        "checked":True
+      },
+      {
+        "time": "19:15",
+        "checked":True
+      },
+      {
+        "time": "19:30",
+        "checked":True
+      },
+      {
+        "time": "19:45",
+        "checked":True
+      },
+      {
+        "time": "20:00",
+        "checked":True
+      },
+      {
+        "time": "20:15",
+        "checked":True
+      },
+      {
+        "time": "20:30",
+        "checked":True
+      },
+      {
+        "time": "20:45",
+        "checked":True
+      },
+      {
+        "time": "21:00",
+        "checked":True
+      },
+      {
+        "time": "21:15",
+        "checked":True
+      },
+      {
+        "time": "21:30",
+        "checked":True
+      },
+      {
+        "time": "21:45",
+        "checked":True
+      },
+      {
+        "time": "22:00",
+        "checked":True
+      },
+      {
+        "time": "22:15",
+        "checked":True
+      },
+      {
+        "time": "22:30",
+        "checked":True
+      },
+      {
+        "time": "22:45",
+        "checked":True
+      },
+      {
+        "time": "23:00",
+        "checked":True
+      },
+      {
+        "time": "23:15",
+        "checked":True
+      },
+      {
+        "time": "23:30",
+        "checked":True
+      },
+      {
+        "time": "23:45",
+        "checked":True
+      }
+    ]
+  },
+  {
+    "dateTitle": "3 / 8 Wednesday",
+    "dates": [
+      {
+        "time": "19:00",
+        "checked":True
+      },
+      {
+        "time": "19:15",
+        "checked":True
+      },
+      {
+        "time": "19:30",
+        "checked":True
+      },
+      {
+        "time": "19:45",
+        "checked":True
+      },
+      {
+        "time": "20:00",
+        "checked":True
+      },
+      {
+        "time": "20:15",
+        "checked":True
+      },
+      {
+        "time": "20:30",
+        "checked":True
+      },
+      {
+        "time": "20:45",
+        "checked":True
+      },
+      {
+        "time": "21:00",
+        "checked":True
+      },
+      {
+        "time": "21:15",
+        "checked":True
+      },
+      {
+        "time": "21:30",
+        "checked":True
+      },
+      {
+        "time": "21:45",
+        "checked":True
+      },
+      {
+        "time": "22:00",
+        "checked":True
+      },
+      {
+        "time": "22:15",
+        "checked":True
+      },
+      {
+        "time": "22:30",
+        "checked":True
+      },
+      {
+        "time": "22:45",
+        "checked":True
+      },
+      {
+        "time": "23:00",
+        "checked":True
+      },
+      {
+        "time": "23:15",
+        "checked":True
+      },
+      {
+        "time": "23:30",
+        "checked":True
+      },
+      {
+        "time": "23:45",
+        "checked":True
+      }
+    ]
+  }
+]
